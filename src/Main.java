6b5911c7289542b3dbd0f2e25e46a2882596000b
Doan Thanh Phat <doanphatwin@gmail.com>
2025-12-26 21:10:19 +0700

    chore: delete useless file

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import common.Message;
-import common.MessageType;
-
-public class Main {
-    public static void main(String[] args) {
-        Message msg = new Message(MessageType.LOGIN, "Client-01", "Hello Server", "Watch");
-        System.out.println("Tạo thử tin nhắn: " + msg.toString());
-    }
-}
